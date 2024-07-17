0bb71f3c94976e021a99653f9d319aa9475bf32f
Yusuf BULUT <131755112+Muyubu1@users.noreply.github.com>
2024-07-17 08:55:09 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public class MyDictionary<TKey,TValue>
+{
+    private List<TKey> _keys;
+    private List<TValue> _values;
+
+    public MyDictionary()
+    {
+        _keys = new List<TKey>();
+        _values = new List<TValue>();
+    }
+
+    public void Add(TKey key, TValue value){
+        if (_keys.Contains(key))
+        {
+            throw new ArgumentException("An item with the same key has already been added.");
+        }
+        _keys.Add(key);
+        _values.Add(value);
+    }
+}
+//Kendi sözlük yapımı tasarladım ve buna ekleme metodu yazdım 
